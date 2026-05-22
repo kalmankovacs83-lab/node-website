b7cf6a1d0dad9009a0e6ab6f15b1ade27042c110
Kálmán <kalman.kovacs83@gmail.com>
2026-05-22 20:01:26 +0200

    Express installed and prepared

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Ready");
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (request, response) => {
+    response.send('<link rel="stylesheet" href="style.css"><h1>Hello!</h1>');
+});
+
+app.listen(8888);
