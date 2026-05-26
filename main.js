12e13ace82245d7c3895ce001377348eb777c5dd
Kálmán <kalman.kovacs83@gmail.com>
2026-05-26 17:16:09 +0200

    Link and Routes - Example

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (request, response) => {
-    response.send('<link rel="stylesheet" href="style.css"><h1>Hello!</h1>');
+    response.send('<link rel="stylesheet" href="style.css"><h1>Hello!</h1><a href="/valami">Valami</a>');
+});
+app.get('/valami', (request, response) => {
+    response.send('<link rel="stylesheet" href="style.css"><h1>Valami!</h1><a href="/">Kezdőlap</a>');
 });
 
 app.listen(8888);
