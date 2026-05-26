239e163ba8b96561413e8896f1fb3eb37b4980d2
Kálmán <kalman.kovacs83@gmail.com>
2026-05-26 17:47:27 +0200

    Simple EJS-Render example

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (request, response) => {
-    response.send('<link rel="stylesheet" href="style.css"><h1>Hello!</h1><a href="/valami">Valami</a>');
+    const ejs = require('ejs');
+    const template = '<link rel="stylesheet" href="style.css"><h1><%= title %></h1><p><%= text %></p><a href="/valami">Valami</a>';
+    const model = {title: 'Kezdőlap', text: 'Ez pedig a főoldal tartalmi része'};
+    const html = ejs.render(template, model);
+    response.send(html);
 });
 app.get('/valami', (request, response) => {
     response.send('<link rel="stylesheet" href="style.css"><h1>Valami!</h1><a href="/">Kezdőlap</a>');
