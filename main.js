0f027186988a40b6ba886a8b02cf351e6e5f50eb
Kálmán <kalman.kovacs83@gmail.com>
2026-05-26 18:44:37 +0200

    EJS: Pages + Partial Views

--- main.js
+++ main.js
@@ -6,11 +6,19 @@ app.use(express.static('public') );
 
 app.get('/', (request, response) => {
     const model = {title: 'Üdvözlünk!'};
-    response.render('home', model);
+    response.render('pages/home', model);
 });
-app.get('/valami', (request, response) => {
-    const model = {title: 'Valami'};
-    response.render('home', model);
+app.get('/szolgaltatasok', (request, response) => {
+    const model = {title: 'Szolgáltatásaink'};
+    response.render('pages/services', model);
+});
+app.get('/cikkek', (request, response) => {
+    const model = {title: 'Friss hírek'};
+    response.render('pages/articles', model);
+});
+app.get('/kapcsolat', (request, response) => {
+    const model = {title: 'Elérhetőségeink'};
+    response.render('pages/home', model);
 });
 
 app.listen(8888);
